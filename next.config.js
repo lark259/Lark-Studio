09dd59ea54b6b936f62673e8a6b38d3d2d092117
lark259 <sofu234@naver.comgit config --global user.email sofu234@naver.com>
2025-04-22 00:45:51 +0900

    Fix: Update Next.js image configuration for logo display

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
